1b94da6705029fab21a6ed4f8f8f9ddf91080986
roronya <roronya628@gmail.com>
2025-09-14 16:36:17 +0900

    check magic number

--- src/main/java/makejvm/ClassFile/ClassFile.java
+++ src/main/java/makejvm/ClassFile/ClassFile.java
@@ -1,7 +1,23 @@
 package makejvm.ClassFile;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 public class ClassFile {
-    String hello() {
-        return "Hello";
+    private static final long MAGIC_NUMBER = 0xCAFEBABEL;
+
+    public static ClassFile read(InputStream cfInputStream) throws IOException {
+        byte[] allBytes = cfInputStream.readAllBytes();
+
+        ByteBuffer buffer = ByteBuffer.wrap(allBytes);
+        buffer.order(ByteOrder.BIG_ENDIAN);
+
+        long magic = buffer.getInt() & 0xFFFFFFFFL;
+        if (magic != MAGIC_NUMBER) {
+            throw new IOException("invalid magic number");
+        }
+        return null;
     }
 }
--- src/test/java/makejvm/ClassFile/ClassFileTest.java
+++ src/test/java/makejvm/ClassFile/ClassFileTest.java
@@ -1,14 +1,31 @@
 package makejvm.ClassFile;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 class ClassFileTest {
-    @Test
-    void hello() {
-        String hello = new ClassFile().hello();
-        assertEquals(hello, "Hello");
+    @Nested
+    class read {
+        @Test
+        void validMagicNumber() {
+            byte[] bytes = {(byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE};
+            InputStream inputStream = new ByteArrayInputStream(bytes);
+
+            Assertions.assertDoesNotThrow(() -> ClassFile.read(inputStream));
+        }
+
+        @Test
+        void invalidMagicNumber() {
+            byte[] bytes = {(byte) 0xAA, (byte) 0xBB, (byte) 0xCC, (byte) 0xDD};
+            InputStream inputStream = new ByteArrayInputStream(bytes);
+
+            Assertions.assertThrows(IOException.class, () -> ClassFile.read(inputStream));
+        }
     }
 
 }
